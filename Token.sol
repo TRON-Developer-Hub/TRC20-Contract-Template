2d919749c71bf93981ed81d2ccd421cd593c798e
Hola2288 <93285198+Hola2288@users.noreply.github.com>
2021-10-28 00:27:34 +0530

    updates coin name , number and coin code

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is TRC20, TRC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public TRC20Detailed("YourTokenName", "YTN", 18) {
-        _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
+    constructor () public TRC20Detailed("Hola Coin", "HAC", 18) {
+        _mint(msg.sender, 3000000000 * (10 ** uint256(decimals())));
     }
 }
